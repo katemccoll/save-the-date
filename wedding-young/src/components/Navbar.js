6f24fd65cfda1ad6f18e7f006e3240b346551ac3
Kate McColl <kate@katemccollart.com>
2021-10-14 09:35:03 +1300

    change: navbar to include links

--- wedding-young/src/components/Navbar.js
+++ wedding-young/src/components/Navbar.js
@@ -24,29 +24,30 @@ const Navbar = () => {
 
     return (
         <>
-            <div className="navbar">
+            <div className={click ? 'navbar active' : 'navbar'}>
                 <div className="navbar-container">
+                    <h1 className="nav-title">Kate and Campbell</h1>
                     <div className="menu-icon" onClick={handleClick}>
                         {click ? <FaTimes /> : <FaBars />}
                     </div>
                     <ul className={click ? 'nav-menu active' : 'nav-menu'}>
                         <li className="nav-item">
                             <Link to='/'
-                                  className="nav-links"
+                                  className="nav-link"
                                   onClick={closeMobileMenu}>
-                                Home
+                                Welcome
                             </Link>
                         </li>
                         <li className="nav-item">
-                            <Link to='/'
-                                  className="nav-links"
+                            <Link to='/details'
+                                  className="nav-link"
                                   onClick={closeMobileMenu}>
                                 Details
                             </Link>
                         </li>
                         <li className="nav-btn">
                             {button ? (
-                                <Link to='/rsvp' className="nav-links">
+                                <Link to='/rsvp' className="nav-link">
                                     <Button buttonStyle='btn--outline'>RSVP</Button>
                                 </Link>
                             ) : (
