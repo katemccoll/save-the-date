e4f1c3e3fd73e043dd3b37f7e660eb9a2ff26b33
Kate McColl <kate@katemccollart.com>
2021-10-24 11:10:40 +1300

    add: details

---
+++ wedding-young/src/components/Registry.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Registry.css";
+
+const Registry = () => {
+    return (
+        <>
+            <div className="registry-container">
+                <h1 className="registry-title">Registry</h1>
+                <p className="registry-content">
+                    We are excited to start our lives together, and we are thankful
+                    for our loved ones. As we enter our marriage, we realize that
+                    we have enough belongings. Therefore, we have decided to plan
+                    the honeymoon of our dreams. Our wedding registry includes a
+                    honeymoon fund along with personalized experiences — such as
+                    excursions, date nights, and spa days — that can be given as wedding
+                    gifts. We are excited to share our special day with you, and
+                    we also look forward to providing glimpses into our honeymoon
+                    when we return. Thank you!</p>
+            </div>
+        </>
+    )
+}
+
+export default Registry;
