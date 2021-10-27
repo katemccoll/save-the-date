7f91ed66d709a6bcd904302b7fc79cb5674ae001
Kate McColl <kate@katemccollart.com>
2021-10-27 13:32:18 +1300

    add: new images

--- wedding-young/src/components/Gallery.js
+++ wedding-young/src/components/Gallery.js
@@ -1,32 +1,15 @@
-import React, {useState} from "react";
+import React from "react";
 import "./Gallery.css";
 
-
-
 const Gallery = () => {
-    const gallery = document.querySelectorAll('.image');
-
-    function toggleOpen() {
-        console.log('hello');
-        this.classList.toggle('open')
-    }
-
-    function toggleActive(e) {
-        console.log(e.propertyName);
-        if (e.propertyName.includes('flex')) {
-            this.classList.toggle('open-active');
-        }
-    }
-
-    gallery.forEach(image => image.addEventListener('click', toggleOpen));
-    gallery.forEach(image => image.addEventListener('transitionend', toggleActive));
-
-
-    return (
+       return (
         <div className="gallery">
             <div className="image image1"></div>
             <div className="image image2"></div>
             <div className="image image3"></div>
+            <div className="image image4"></div>
+            <div className="image image5"></div>
+            <div className="image image6"></div>
         </div>
     )
 
