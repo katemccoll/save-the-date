a8b4aa9105e290b07647de5e05cce2c6ee33fcab
Kate McColl <kate@katemccollart.com>
2021-11-02 11:28:53 +1300

    change: information; remove: unwanted titles

--- wedding-young/src/components/Information.js
+++ wedding-young/src/components/Information.js
@@ -1,31 +1,28 @@
 import React from "react";
 import './Information.css';
-import {Button} from "./Button";
-import {Link} from "react-router-dom";
 
 const Information = () => {
     return (
         <>
             <div id='information' className="information-container">
                 <div className="information-image"></div>
-                <h1 className="information-title">Information</h1>
-                <p>
-                    We are excited for you to come and join us celebrate our big day. We have a few final things to sort
-                    before we send our RSVPs out. But while you wait, here is some information to tie you over.
-                </p>
                 <div className="information-wrapper">
-                    <div className="information-content">
-                        <h3 className="transport-title">Transport</h3>
-                        <p>A 17 minute drive from Cromwell. There is a carpark on site if you wish to drive, otherwise
-                        there will be a shuttle between the Five Stags and The Canyon</p>
-                    </div>
-                    <div className="information-content">
-                        <h3 className="accommodation-title">Accommodation</h3>
-                        <p>There is a range of places to stay in Cromwell</p>
-                    </div>
+                    <h1 className="information-title">Information</h1>
+                    <p className="information-intro">
+                        We are excited for you to join us to celebrate our special day.
+                    </p>
+
+                    <p className="information-intro">
+                        The Canyon is a 17 minute drive from Cromwell. There is a carpark on site if you wish to drive,
+                        otherwise there will be a shuttle between the Five Stags and The Canyon.
+                        We suggest booking your accommodation in Cromwell.
+                    </p>
+
+                    <p className="information-intro">
+                        We will be updating this website with more details closer to the time.
+                    </p>
                 </div>
             </div>
-
         </>
     )
 }
